6b32829a6044bf84600f372898a18b2ae2b0b4e9
shuvamshr <shuvamshr@outlook.com>
2026-03-18 16:51:55 +1100

    Initial Commit

---
+++ TodoWed1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoWed1
+//
+//  Created by Shuvam Shrestha on 18/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoWed1/TodoWed1App.swift
@@ -0,0 +1,17 @@
+//
+//  TodoWed1App.swift
+//  TodoWed1
+//
+//  Created by Shuvam Shrestha on 18/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct TodoWed1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
